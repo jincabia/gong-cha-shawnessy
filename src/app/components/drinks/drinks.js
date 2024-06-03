92feb00e8a857b787e05d2b9476ba44790c98a8b
jincabia <140627796+jincabia@users.noreply.github.com>
2024-06-03 11:47:00 -0600

    Added Dynamic Image pathing

--- src/app/components/drinks/drinks.js
+++ src/app/components/drinks/drinks.js
@@ -8,24 +8,37 @@ import Image from 'next/image';
 // TODO onclick component then customize drink then add to cart then order
 const Drink = ({ name, price }) => {
 
+  // Milk Tea w/ Pearls
 
+// to have custom imgs set all photos to the name var? for examples EGMT with 3J img = name maybe
 
   return (
-    <div className='w-full sm:w-64 h-fit p-4 rounded-lg shadow-lg flex flex-col items-center justify-between text-center hover:drop-shadow-xl'>
-      <div className='flex justify-center items-center'>
-        <Image 
-          src={'/Milk Tea w_ Pearls.png'} 
-          width={100} 
-          height={100} 
-          className='sm:w-28 sm:h-38 md:w-32 md:h-48 lg:w-30 lg:h-48' 
-          alt={name} 
-        />
+    <div>
+
+      <div className='w-full sm:w-64 h-fit p-4 rounded-lg shadow-lg flex flex-col items-center justify-between text-center hover:drop-shadow-xl' >
+        <div className='flex justify-center items-center'>
+          <Image 
+            // src={'/Milk Tea w_ Pearls.png'} 
+            src={`/${name}.png`} 
+            width={100} 
+            height={100} 
+            className='sm:w-28 sm:h-38 md:w-32 md:h-48 lg:w-30 lg:h-48' 
+            alt={name} 
+          />
+        </div>
+
+
+
+       
       </div>
       <div className='flex flex-col items-start justify-start flex-grow mt-4'>
-        <h1 className='text-black font-semibold w-fit text-xs mb-2 '>{name}</h1>
-        <p className='text-black text-xs'>${price}</p>
-      </div>
+          <h1 className='text-black font-semibold w-fit text-xs mb-2 truncate '>{name}</h1>
+          <p className='text-black text-sm'>${price}</p>
+        </div>
+
+
     </div>
+
   );
 };
 
