43bd7b2e8c9420bb3339d2377f026df0800fce8b
BraydonAnderson1 <129642678+BraydonAnderson1@users.noreply.github.com>
2024-06-05 13:20:46 -0600

    created orders page

---
+++ src/app/orders/page.js
@@ -0,0 +1,13 @@
+import OrdersList from "../components/drinks/drinksList";
+import OrdersList from "./orderlist";
+
+export default function Menu() {
+  return (
+    <main className="flex-grow">
+
+      <h1 className="text-black">Fart</h1>
+
+      <OrdersList/>
+    </main>
+  );
+}
