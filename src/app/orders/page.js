cf765f54c255da84599ccf5546dbd31ef0602b5b
jincabia <140627796+jincabia@users.noreply.github.com>
2024-06-14 10:29:01 -0600

    Removed Orderspage

--- src/app/orders/page.js
+++ src/app/orders/page.js
@@ -4,9 +4,6 @@ import OrdersList from "./orderlist";
 export default function Menu() {
   return (
     <main className="flex-grow">
-
-      <h1 className="text-black">Fart</h1>
-
       <OrdersList/>
     </main>
   );
