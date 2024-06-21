a8466ea2b540b0558c8728cc9154c97f0f818c64
lei.caps <leinajozsa.capili@edu.sait.ca>
2024-06-21 13:49:24 -0600

    leina changes

--- src/app/components/homepage/ourstory.js
+++ src/app/components/homepage/ourstory.js
@@ -1,11 +1,18 @@
 "use client";
+import { useRouter } from "next/navigation";
 
 const OurStory = () => {
+    const router = useRouter();
+
+    const navToStore = () => {
+        router.push("/store");
+      };
+
     return (
         <main className="flex flex-col text-black min-h-screen items-center text-center pt-72 px-10">
             <p className="text-red-800 tracking-widest">OUR STORY</p>
             <h1 className="text-4xl font-serif py-5">Brewing happiness</h1>
-            <button className="bg-red-800 rounded-md py-2 px-4 text-white shadow-md">READ MORE</button>
+            <button className="bg-red-800 rounded-md py-2 px-4 text-white shadow-md" onClick={navToStore}>READ MORE</button>
         </main>
     );
 };
