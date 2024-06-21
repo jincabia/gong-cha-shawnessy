4c2522fef6181d71fc8cfdfec89fe74c4375e363
jincabia <140627796+jincabia@users.noreply.github.com>
2024-06-21 13:58:45 -0600

    Added photos to the home page, might need to work on navigation

--- src/app/components/homepage/ourstore.js
+++ src/app/components/homepage/ourstore.js
@@ -1,12 +1,28 @@
 "use client";
+import { useRouter } from "next/navigation";
 
 const OurStore = () => {
+    const router = useRouter();
+
+    const navToStore = () => {
+        router.push("/menu");
+    };
+    // return (
+    //     <main className="flex flex-col items-center text-center text-black min-h-screen pt-64 px-10">
+    //         <p className="text-red-800 tracking-widest">OUR STORE</p>
+    //         <h1 className="text-4xl font-serif py-5">Drop by to pick up your drink!</h1>
+    //         <button className="bg-red-800 rounded-md py-2 px-4 text-white shadow-md">VIEW MORE</button>
+    //     </main>
+    // );
+
     return (
-        <main className="flex flex-col items-center text-center text-black min-h-screen pt-64 px-10">
-            <p className="text-red-800 tracking-widest">OUR STORE</p>
-            <h1 className="text-4xl font-serif py-5">Drop by to pick up your drink!</h1>
-            <button className="bg-red-800 rounded-md py-2 px-4 text-white shadow-md">VIEW MORE</button>
-        </main>
+        <div className="min-h-screen bg-cover bg-center" style={{ backgroundImage: "url('/Tea-25-scaled.jpg')" }}>
+            <main className="flex flex-col items-center justify-center text-white text-center min-h-screen bg-black bg-opacity-50">
+                <p className="text-red-800 tracking-widest">OUR STORE</p>
+                <h1 className="text-4xl font-serif py-5">Drop by to pick up your drink!</h1>
+                <button className="bg-red-800 rounded-md py-2 px-4 text-white shadow-md" onClick={navToStore}>VIEW MORE</button>
+            </main>
+        </div>
     );
 };
 
