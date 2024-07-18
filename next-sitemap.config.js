36bfec61c5da00e0fabf4960d33734ce1a72687f
jincabia <140627796+jincabia@users.noreply.github.com>
2024-07-18 16:37:09 -0600

    site mapping cont

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,15 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://gongcha-shawnessy.vercel.app',
-    generateRobotsTxt: true, // (optional)
-    // other options
-  }
+    siteUrl: 'https://gongcha-shawnessy.vercel.app/',
+    generateRobotsTxt: true, // (Optional) Generate a robots.txt file
+    changefreq: 'daily',
+    priority: 0.7,
+    sitemapSize: 5000,
+    exclude: [''], // Optional: Exclude specific pages
+    robotsTxtOptions: {
+      additionalSitemaps: [
+        // 'https://yourwebsite.com/sitemap-0.xml', // If you have additional sitemaps
+      ],
+    },
+  };
   
