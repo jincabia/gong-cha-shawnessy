ac434e383d50f04c12ed9b819e5c9f77ec476af3
jincabia <140627796+jincabia@users.noreply.github.com>
2024-07-25 01:41:15 -0600

    forgot to add one condition

--- src/app/menu/NewAdditions.js
+++ src/app/menu/NewAdditions.js
@@ -7,15 +7,6 @@ const NewAdditions = () =>
     return(
         <div className="">
 
-            {/* <div className='text-center w-11/12 mx-auto mb-5'>
-                <div className='flex items-center justify-between pb-5'>
-                <div className='flex-grow border-t border-slate-300'></div>
-                <span className='mx-4 text-2xl text-red-800  font-serif'>New Additions</span>
-                <div className='flex-grow border-t border-slate-300'></div>
-                </div>
-                <h1 className='text-black'></h1>
-            </div> */}
-
             <div className="mx-auto text-center justify-center  " >
                 <Image
                     src="/Final-Fantasy_TV_EN-H-no-price.jpg"
